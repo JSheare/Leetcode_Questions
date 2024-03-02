7c621c23c66508b56774bfbdc9e59cc1ae880cde
JSheare <jrsheare@ucsc.edu>
2024-03-01 21:47:37 -0800

    Added binary tree class for easier testing

--- Leetcode_Questions/226_Invert_Binary_Tree.cpp
+++ Leetcode_Questions/226_Invert_Binary_Tree.cpp
@@ -22,6 +22,84 @@ struct TreeNode
 	{}
 };
 
+
+class BST
+{
+public:
+	TreeNode* root;
+	std::vector<int> values;
+
+	BST(std::initializer_list<int> values) :
+		root(nullptr), values(values)
+	{
+		this->root = makeTree(this->values);
+	}
+
+	~BST()
+	{
+		deleteTree(this->root);
+	}
+
+	bool operator==(BST& tree2)
+	{
+		return isSameTree(this->root, tree2.root);
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, BST& tree)
+	{
+		out << "[ ";
+		for (int value : tree.values)
+		{
+			out << value << ' ';
+
+		}
+		out << ']';
+		return out;
+	}
+
+	TreeNode* makeTree(std::vector<int>& values)
+	{
+		if (values.size() - 1 == -1)
+			return nullptr;
+
+		return makeNode(values, 0);
+	}
+
+	TreeNode* makeNode(std::vector<int>& values, int index)
+	{
+		int n{ static_cast<int>(values.size()) };
+		if (n <= index)
+			return nullptr;
+
+		TreeNode* node{ new TreeNode{values[index]} };
+		node->left = makeNode(values, 2 * index + 1);
+		node->right = makeNode(values, 2 * index + 2);
+		return node;
+	}
+
+	void deleteTree(TreeNode* root)
+	{
+		if (!root)
+			return;
+
+		deleteTree(root->left);
+		deleteTree(root->right);
+		delete root;
+	}
+
+	bool isSameTree(TreeNode* root1, TreeNode* root2)
+	{
+		if (!root1 && !root2)
+			return true;
+		else if (!root1 || !root2)
+			return false;
+
+		return (root1->val == root2->val) &&
+			isSameTree(root1->left, root2->left) &&
+			isSameTree(root1->right, root2->right);
+	}
+};
+
 TreeNode* invertTree(TreeNode* root)
 {
 	if (!root)
@@ -36,83 +114,24 @@ TreeNode* invertTree(TreeNode* root)
 	return root;
 }
 
-TreeNode* makeNode(std::vector<int>& values, int index)
-{
-	int n{ static_cast<int>(values.size()) };
-	if (n <= index)
-		return nullptr;
-
-	TreeNode* node{ new TreeNode{values[index]} };
-	node->left = makeNode(values, 2 * index + 1);
-	node->right = makeNode(values, 2 * index + 2);
-	return node;
-}
-
-TreeNode* makeTree(std::vector<int>& values)
-{
-	if (values.size() - 1 == -1)
-		return nullptr;
-
-	return makeNode(values, 0);
-}
-
-void deleteTree(TreeNode* root)
-{
-	if (!root)
-		return;
-
-	deleteTree(root->left);
-	deleteTree(root->right);
-	delete root;
-}
-
-bool isSameTree(TreeNode* root1, TreeNode* root2)
-{
-	if (!root1 && !root2)
-		return true;
-	else if (!root1 || !root2)
-		return false;
-
-	return (root1->val == root2->val) && 
-		isSameTree(root1->left, root2->left) && 
-		isSameTree(root1->right, root2->right);
-}
-
 # if 0
 int main()
 {
-	TreeNode* testTree;
-	TreeNode* correctAnswerTree;
-	TreeNode* answerTree;
-
 	// Provided test cases
-	std::vector<int> test1{ 4, 2, 7, 1, 3, 6, 9 };
-	std::vector<int> answer1{ 4, 7, 2, 9, 6, 3, 1 };
-	testTree = makeTree(test1);
-	correctAnswerTree = makeTree(answer1);
-	answerTree = invertTree(testTree);
-	assert(isSameTree(answerTree, answerTree));
-	deleteTree(correctAnswerTree);
-	deleteTree(answerTree);
+	BST testTree1{ 4, 2, 7, 1, 3, 6, 9 };
+	BST answerTree1{ 4, 7, 2, 9, 6, 3, 1 };
+	invertTree(testTree1.root);
+	assert(testTree1 == answerTree1);
 
-	std::vector<int> test2{ 2, 1, 3 };
-	std::vector<int> answer2{ 2, 3, 1 };
-	testTree = makeTree(test2);
-	correctAnswerTree = makeTree(answer2);
-	answerTree = invertTree(testTree);
-	assert(isSameTree(answerTree, answerTree));
-	deleteTree(correctAnswerTree);
-	deleteTree(answerTree);
+	BST testTree2{ 2, 1, 3 };
+	BST answerTree2{ 2, 3, 1 };
+	invertTree(testTree2.root);
+	assert(testTree2 == answerTree2);
 
-	std::vector<int> test3{};
-	std::vector<int> answer3{};
-	testTree = makeTree(test3);
-	correctAnswerTree = makeTree(answer3);
-	answerTree = invertTree(testTree);
-	assert(isSameTree(answerTree, answerTree));
-	deleteTree(correctAnswerTree);
-	deleteTree(answerTree);
-
+	BST testTree3{};
+	BST answerTree3{};
+	invertTree(testTree3.root);
+	assert(testTree3 == answerTree3);
 
 	std::cout << "Success!\n";
 
