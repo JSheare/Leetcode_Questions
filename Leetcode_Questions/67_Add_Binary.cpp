01571a77c63c33a4aa2701bd97a316a5a237a1ea
JSheare <jrsheare@ucsc.edu>
2024-03-20 12:40:40 -0700

    Added add binary q

---
+++ Leetcode_Questions/67_Add_Binary.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include <algorithm>
+
+std::string addBinary(std::string a, std::string b)
+{
+    std::string result;
+    int i{ static_cast<int>(a.length()) - 1 };
+    int j{ static_cast<int>(b.length()) - 1 };
+    int carry{ 0 };
+    while (i >= 0 || j >= 0 || carry)
+    {
+        if (i >= 0)
+        {
+            carry += a[i] - '0';
+            --i;
+        }
+        if (j >= 0)
+        {
+            carry += b[j] - '0';
+            --j;
+        }
+
+        result += carry % 2 + '0';
+        carry /= 2;
+    }
+    reverse(begin(result), end(result));
+    return result;
+}
+
+# if 0
+int main()
+{
+    // Provided test cases
+    assert(addBinary("11", "1") == "100");
+
+    assert(addBinary("1010", "1011") == "10101");
+
+    std::cout << "Success!\n";
+
+	return 0;
+}
+#endif
