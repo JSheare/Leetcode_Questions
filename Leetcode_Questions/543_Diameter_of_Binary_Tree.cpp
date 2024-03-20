1007dd7db3d0f93215cc1efddbf0cea893606619
JSheare <jrsheare@ucsc.edu>
2024-03-20 13:09:29 -0700

    Added diameter of binary tree q

---
+++ Leetcode_Questions/543_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+#include "TreeNode.h"
+#include "BinaryTree.h"
+
+int height(TreeNode* root, int& diameter)
+{
+    if (!root)
+        return 0;
+
+    int left{ height(root->left, diameter) };
+    int right{ height(root->right, diameter) };
+    diameter = std::max(left + right, diameter);
+    return 1 + std::max(left, right);
+}
+
+int diameterOfBinaryTree(TreeNode* root)
+{
+    if (!root)
+        return 0;
+
+    int diameter{ 0 };
+    height(root, diameter);
+    return diameter;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    BinaryTree test1{ "1", "2", "3", "4", "5"};
+    assert(diameterOfBinaryTree(test1.root) == 3);
+
+    BinaryTree test2{ "1", "2" };
+    assert(diameterOfBinaryTree(test2.root) == 1);
+
+    std::cout << "Success!\n";
+
+	return 0;
+}
+#endif
