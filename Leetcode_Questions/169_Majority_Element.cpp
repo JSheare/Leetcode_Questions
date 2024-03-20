2295a22ec65fc3c95a71ea778d885858673e34b5
JSheare <jrsheare@ucsc.edu>
2024-03-20 11:52:19 -0700

    Added majority element q

---
+++ Leetcode_Questions/169_Majority_Element.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+// This is the Boyer-Moore majority vote algorithm
+int majorityElement(std::vector<int>& nums)
+{
+    int count{ 0 };
+    int candidate{ 0 };
+    for (int num : nums)
+    {
+        if (!count)
+            candidate = num;
+
+        if (num == candidate)
+            ++count;
+        else
+            --count;
+    }
+    return candidate;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    std::vector test1{ 3, 2, 3 };
+    assert(majorityElement(test1) == 3);
+
+    std::vector test2{ 2, 2, 1, 1, 1, 2, 2 };
+    assert(majorityElement(test2) == 2);
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
