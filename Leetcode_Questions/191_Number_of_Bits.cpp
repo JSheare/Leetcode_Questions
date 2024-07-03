43bb3ea33c8ac8ae5ca5e93603c46996e00c6d8c
JSheare <jrsheare@ucsc.edu>
2024-07-03 11:33:07 -0700

    Added hamming weight q

---
+++ Leetcode_Questions/191_Number_of_Bits.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <assert.h>
+
+int hammingWeight(int n)
+{
+    int counter{ 0 };
+    while (n)
+    {
+        if (n & 1)
+            ++counter;
+
+        n = n >> 1;
+    }
+    return counter;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    assert(hammingWeight(11) == 3);
+
+    assert(hammingWeight(128) == 1);
+
+    assert(hammingWeight(2147483645) == 30);
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
