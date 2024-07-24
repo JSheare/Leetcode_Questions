b9fbbbe7c3a178a0713e930e2389da610b034046
JSheare <jrsheare@ucsc.edu>
2024-07-24 14:45:41 -0700

    Added palindrome number q

---
+++ Leetcode_Questions/9_Palindrome_Number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <assert.h>
+
+bool isPalindrome(int x)
+{
+    if (x < 0 || (x != 0 && x % 10 == 0))
+        return false;
+
+    int reversed{ 0 };
+    while (x > reversed)
+    {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+    return (x == reversed) || (x == reversed / 10);
+}
+
+#if 0
+int main()
+{
+    //Provided test cases
+    assert(isPalindrome(121) == true);
+
+    assert(isPalindrome(-121) == false);
+
+    assert(isPalindrome(10) == false);
+
+    std::cout << "Success!";
+
+    return 0;
+}
+#endif
