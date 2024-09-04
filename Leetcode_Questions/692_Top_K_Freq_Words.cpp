aec5ae53c41e14c416d61bd31a57c560ac1d7b17
JSheare <jrsheare@ucsc.edu>
2024-09-04 12:24:31 -0700

    Added top k frequent words q

---
+++ Leetcode_Questions/692_Top_K_Freq_Words.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+
+std::vector<std::string> topKFrequent(std::vector<std::string>& words, int k)
+{
+    std::unordered_map<std::string, int> freq;
+    for (auto& word : words)
+    {
+        ++freq[word];
+    }
+    auto comp
+    {
+        [](std::pair<std::string, int>& a, std::pair<std::string, int>& b)
+        {return (a.second > b.second) || ((a.second == b.second) && (a.first < b.first)); }
+    };
+    std::vector<std::pair<std::string, int>> ordered;
+    for (auto& it : freq)
+    {
+        ordered.push_back(it);
+    }
+    std::sort(ordered.begin(), ordered.end(), comp);
+    std::vector<std::string> topK;
+    for (int i{ 0 }; i < k; ++i)
+    {
+        topK.push_back(ordered[i].first);
+    }
+    return topK;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    std::vector<std::string> test1{ "i", "love", "leetcode", "i", "love", "coding" };
+    std::vector<std::string> correct1{ "i", "love" };
+    assert(topKFrequent(test1, 2) == correct1);
+
+    std::vector<std::string> test2{ "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+    std::vector<std::string> correct2{ "the", "is", "sunny", "day" };
+    assert(topKFrequent(test2, 4) == correct2);
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
