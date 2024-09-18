acef8c8415d4d065e4d392314e6117cd6563533b
JSheare <jrsheare@ucsc.edu>
2024-09-18 14:47:43 -0700

    Added rotate array q

---
+++ Leetcode_Questions/189_Rotate_Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+void rotate(std::vector<int>& nums, int k)
+{
+    k = k % nums.size();
+    if (k == 0)
+        return;
+
+    reverse(nums.begin(), nums.begin() + (nums.size() - k));
+    reverse(nums.begin() + (nums.size() - k), nums.end());
+    reverse(nums.begin(), nums.end());
+}
+
+#if 
+int main()
+{
+    // Provided test cases
+    std::vector<int> test1{ 1, 2, 3, 4, 5, 6, 7 };
+    std::vector<int> correct1{ 5, 6, 7, 1, 2, 3, 4 };
+    rotate(test1, 3);
+    assert(test1 == correct1);
+
+    std::vector<int> test2{ -1, -100, 3, 99 };
+    std::vector<int> correct2{ 3, 99, -1, -100 };
+    rotate(test2, 2);
+    assert(test2 == correct2);
+    
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
