0b647cc02b2765c331bb1d6c46ee406853cb0725
JSheare <jrsheare@ucsc.edu>
2024-10-16 16:15:29 -0700

    Added jumping game q

---
+++ Leetcode_Questions/55_Jump_Game.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+bool canJump(std::vector<int>& nums)
+{
+    int remainingJumps{ 0 };
+    for (auto& num : nums)
+    {
+        if (remainingJumps < 0)
+            return false;
+
+        if (num > remainingJumps)
+            remainingJumps = num;
+
+        --remainingJumps;
+    }
+    return true;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    std::vector test1{ 2, 3, 1, 1, 4 };
+    assert(canJump(test1) == true);
+
+    std::vector test2{ 3, 2, 1, 0, 4 };
+    assert(canJump(test2) == false);
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
