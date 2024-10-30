e8cb5c5dd9befd0d3fa9d3ce249df8dbcd88dca9
JSheare <jrsheare@ucsc.edu>
2024-10-30 16:15:55 -0700

    Added random pick with weight q

---
+++ Leetcode_Questions/528_Random_Pick_With_Weight.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+#include "RandomPickWeighted.h"
+
+// See RandomPick.h for implementation
+
+#if 0
+int main()
+{
+	// Provided test cases
+	std::vector test1{ 1 };
+	RandomPickWeighted pick1{ test1 };
+	int index1{ pick1.pickIndex() };
+	assert(index1 == 0);
+
+	std::vector test2{ 1, 3 };
+	RandomPickWeighted pick2{ test2 };
+	int index2{ pick2.pickIndex() };
+	// Index of 1 will be more likely
+	assert(index2 == 0 || index2 == 1);
+
+	std::cout << "Success!\n";
+
+	return 0;
+}
+#endif
