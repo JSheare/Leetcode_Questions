4356b81ce2968be6362043fa5dc24e5cf0b8cd48
Jacob Shearer <jacob.shearer6912@gmail.com>
2026-07-16 14:06:24 -0700

    Added simplify path q

---
+++ Leetcode_Questions/71_Simplify_Path.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cassert>
+#include <deque>
+#include <string>
+
+std::string simplifyPath(std::string path)
+{
+    std::deque<std::string> dq;
+    // Filling the deque with directory/file names
+    for (int i{ 0 }; i < path.size(); ++i)
+    {
+        // Ignoring slashes
+        if (path[i] == '/')
+            continue;
+
+        std::string temp;
+        while (i < path.size() && path[i] != '/')
+        {
+            temp += path[i];
+            ++i;
+        }
+        // Ignoring current directory
+        if (temp == ".")
+            continue;
+        // Popping the previous directory from the deque
+        else if (temp == "..")
+        {
+            if (!dq.empty())
+                dq.pop_back();
+        }
+        else
+            dq.push_back(temp);
+    }
+
+    // Building the simplified canonical path and returning it
+    std::string new_path;
+    while (!dq.empty())
+    {
+        new_path += '/' + dq.front();
+        dq.pop_front();
+    }
+    if (new_path.size() == 0)
+        return "/";
+
+    return new_path;
+}
+
+#if 0
+int main()
+{
+    // Provided test cases
+    assert(simplifyPath("/home/") == "/home");
+
+    assert(simplifyPath("/home//foo/") == "/home/foo");
+
+    assert(simplifyPath("/home/user/Documents/../Pictures") == "/home/user/Pictures");
+
+    assert(simplifyPath("/../") == "/");
+
+    assert(simplifyPath("/.../a/../b/c/../d/./") == "/.../b/d");
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
+#endif
